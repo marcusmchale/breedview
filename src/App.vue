60fe8b5cd817c045f3c127fbdf7c5c59dc36f783
Marcus McHale <marcus.mchale@nuigalway.ie>
2020-10-23 13:00:17 +0100

    Start

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Registration msg="Register" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Registration from "./components/Registration.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    Registration
   }
 };
 </script>
