9ec7a6448ff8340b367d2fccb892fb56bdeff4da
Marcus McHale <marcus.mchale@nuigalway.ie>
2021-03-16 13:48:54 +0000

    it has been a while, coming back to this project so just committing here

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
+import VueFormulate from "@braid/vue-formulate";
+import { createProvider } from "./vue-apollo";
 
 Vue.config.productionTip = false;
+Vue.use(VueFormulate);
 
 new Vue({
+  apolloProvider: createProvider(),
   render: h => h(App)
 }).$mount("#app");
